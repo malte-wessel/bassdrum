78145d1adb65fd2c3fa54a738ff2cc5784f82409
Malte Wessel <malte-wessel@users.noreply.github.com>
2019-11-15 00:44:01 +0100

    add build command

--- src/index.tsx
+++ src/index.tsx
@@ -1,68 +1,9 @@
-import { h, Ref } from 'preact';
-import { map, pluck, tap } from 'rxjs/operators';
-import {
+export {
     ComponentFunction,
     ComponentTemplate,
-    createComponent
+    createComponent,
 } from './component';
-import { combine } from './util/combine';
-import { createHandler } from './util/createHandler';
-import { createRef } from './util/createRef';
-import { MouseEventHandler, MouseEvent } from './types/events';
-
-interface ItemProps {
-    bar: string;
-}
-
-interface ItemState {
-    bar: string;
-    nom: string;
-    foo: number;
-    ref: Ref<HTMLDivElement>;
-    handleClick: MouseEventHandler<HTMLDivElement>;
-}
-
-const ItemFn: ComponentFunction<ItemProps, ItemState> = ({
-    props,
-    subscribe
-}) => {
-    const [ref, elementStream] = createRef<HTMLDivElement>();
-    const [handleClick, clickStream] = createHandler<
-        MouseEvent<HTMLDivElement>
-    >();
-
-    subscribe(
-        elementStream.pipe(
-            tap(el => {
-                el.getBoundingClientRect();
-            })
-        )
-    );
-
-    subscribe(clickStream.pipe(tap(e => e.currentTarget.ATTRIBUTE_NODE)));
-
-    const nom = props.pipe(
-        pluck('bar'),
-        map(val => `${val}!`)
-    );
-    return combine(props, {
-        foo: 1,
-        handleClick,
-        nom,
-        ref
-    });
-};
-
-const ItemTemplate: ComponentTemplate<ItemState> = ({
-    bar,
-    handleClick,
-    ref
-}) => {
-    return (
-        <div onClick={handleClick} ref={ref}>
-            {bar}
-        </div>
-    );
-};
-
-export const Item = createComponent(ItemFn, ItemTemplate);
+export { combine } from './util/combine';
+export { createHandler } from './util/createHandler';
+export { createRef } from './util/createRef';
+export * from './types/events';
