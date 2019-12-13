b6cc8fc313b728d61f228f45ebc89d17ecce12df
Malte Wessel <malte-wessel@users.noreply.github.com>
2019-12-13 17:01:40 +0100

    added tests for lifecycle events

--- src/__tests__/component.tsx
+++ src/__tests__/component.tsx
@@ -1,5 +1,5 @@
 import { h, render, Ref } from 'preact';
-import { tap, delay, filter } from 'rxjs/operators';
+import { first, last, tap, delay, filter } from 'rxjs/operators';
 import {
     createComponent,
     ComponentFunction,
@@ -69,5 +69,54 @@ describe('Component', () => {
 
         const root = document.createElement('div');
         render(<Component />, root);
+    });
+    it('supports lifecylce methods', () => {
+        interface Props {
+            foo: string;
+        }
+
+        interface State {
+            foo: string;
+        }
+
+        const mountedEvents: Props[] = [];
+        const updateEvents: Props[] = [];
+        const unmountedEvents: Props[] = [];
+
+        const cfn: ComponentFunction<Props, State> = ({
+            props,
+            updates,
+            subscribe,
+        }) => {
+            const mounted = updates.pipe(first());
+            const unmount = updates.pipe(last());
+            subscribe(mounted.pipe(tap(p => mountedEvents.push(p))));
+            subscribe(updates.pipe(tap(p => updateEvents.push(p))));
+            subscribe(unmount.pipe(tap(p => unmountedEvents.push(p))));
+            return props;
+        };
+        const ct: ComponentTemplate<State> = state => <div>{state.foo}</div>;
+        const Component = createComponent(cfn, ct);
+
+        const root = document.createElement('div');
+        render(<Component foo="bar" />, root);
+        expect({
+            mountedEvents,
+            updateEvents,
+            unmountedEvents,
+        }).toMatchSnapshot();
+        render(<Component foo="nom" />, root);
+        expect({
+            mountedEvents,
+            updateEvents,
+            unmountedEvents,
+        }).toMatchSnapshot();
+        render(() => null, root);
+        expect({
+            mountedEvents,
+            updateEvents,
+            unmountedEvents,
+        }).toMatchSnapshot();
+        console.log();
     });
 });
