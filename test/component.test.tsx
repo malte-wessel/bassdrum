928443833d6d8a420b6dbe074b9423ed8e32450e
Malte Wessel <malte-wessel@users.noreply.github.com>
2019-12-15 15:30:48 +0100

    added more component tests

--- test/component.test.tsx
+++ test/component.test.tsx
@@ -1,5 +1,5 @@
 import { h, render, Ref } from 'preact';
-import { first, last, tap, delay, filter } from 'rxjs/operators';
+import { first, last, tap, delay, filter, pluck, map } from 'rxjs/operators';
 import {
     createComponent,
     ComponentFunction,
@@ -9,6 +9,7 @@ import { combine } from '../src/util/combine';
 import { createRef } from '../src/util/createRef';
 import { createHandler } from '../src/util/createHandler';
 import { Handler, MouseEvent } from '../src/types/events';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 describe('Component', () => {
     it('renders a component', () => {
@@ -44,6 +45,47 @@ describe('Component', () => {
         expect(() => {
             render(<Component />, root);
         }).toThrow();
+    });
+
+    it('should only rerender when state has changed', () => {
+        interface Props {
+            foo: string;
+            bar: string;
+        }
+
+        interface State {
+            foo: string;
+            size: number;
+        }
+
+        const receives: Props[] = [];
+        const renders: State[] = [];
+        const cfn: ComponentFunction<Props, State> = ({ props, subscribe }) => {
+            subscribe(props.pipe(tap(props => receives.push(props))));
+            const foo = props.pipe(pluck('foo'));
+            const bar = props.pipe(pluck('bar'));
+            const size = bar.pipe(map(bar => bar.length));
+            return combine({
+                foo,
+                size,
+            });
+        };
+        const ct: ComponentTemplate<State> = state => {
+            renders.push(state);
+            return (
+                <div>
+                    {state.foo}
+                    {state.size}
+                </div>
+            );
+        };
+        const Component = createComponent(cfn, ct);
+
+        const root = document.createElement('div');
+        render(<Component foo="bar" bar="nom" />, root);
+        render(<Component foo="bar" bar="mon" />, root);
+        render(<Component foo="bar" bar="mon2" />, root);
+        expect({ receives, renders }).toMatchSnapshot();
     });
 
     it('supports handlers', done => {
@@ -106,6 +148,83 @@ describe('Component', () => {
         const root = document.createElement('div');
         render(<Component />, root);
     });
+
+    it('should manage subscriptions', () => {
+        interface Props {
+            foo: string;
+        }
+
+        interface State {
+            foo: string;
+        }
+
+        const b = new BehaviorSubject('foo');
+
+        const cfn: ComponentFunction<Props, State> = ({ props, subscribe }) => {
+            subscribe(b);
+            return props;
+        };
+        const ct: ComponentTemplate<State> = state => <div>{state.foo}</div>;
+        const Component = createComponent(cfn, ct);
+
+        const root = document.createElement('div');
+        render(<Component foo="bar" />, root);
+        expect(b.observers).toHaveLength(1);
+        render(() => null, root);
+        expect(b.observers).toHaveLength(0);
+    });
+
+    it('should complete props stream on unmount', done => {
+        interface Props {
+            foo: string;
+        }
+
+        interface State {
+            foo: string;
+        }
+
+        let p = new BehaviorSubject<Props>({ foo: 'nom' });
+
+        const cfn: ComponentFunction<Props, State> = ({ props }) => {
+            p = props;
+            return props;
+        };
+        const ct: ComponentTemplate<State> = state => <div>{state.foo}</div>;
+        const Component = createComponent(cfn, ct);
+
+        const root = document.createElement('div');
+        render(<Component foo="bar" />, root);
+        p.subscribe({
+            complete: done,
+        });
+        render(() => null, root);
+    });
+    it('should complete updates stream on unmount', done => {
+        interface Props {
+            foo: string;
+        }
+
+        interface State {
+            foo: string;
+        }
+
+        let u = new Subject<Props>();
+
+        const cfn: ComponentFunction<Props, State> = ({ props, updates }) => {
+            u = updates;
+            return props;
+        };
+        const ct: ComponentTemplate<State> = state => <div>{state.foo}</div>;
+        const Component = createComponent(cfn, ct);
+
+        const root = document.createElement('div');
+        render(<Component foo="bar" />, root);
+        u.subscribe({
+            complete: done,
+        });
+        render(() => null, root);
+    });
+
     it('supports lifecylce methods', () => {
         interface Props {
             foo: string;
